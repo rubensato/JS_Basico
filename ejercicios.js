5e8a00d43a3c52552d9f637bc69955b5dd691376
rubensato <rubensato@gmail.com>
2021-05-18 08:22:38 -0300

    inicio archivo ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,15 @@
+/*****************************************************
+/*****************************************************
+-- Lenguajes de Programación para la Administración --
+
+---------- Ejercicios de JavaScript Basico -----------
+
+Descripción
+-----------
+
+Se deben resolver cada uno de los siguientes ejercicios y subirlos todos juntos a un nuevo repositorio en Github.
+Por favor resolverlos en el orden planteado y hacer un commit por cada ejercicio resuelto.
+Agregar el enunciado de cada ejercicio como comentario en la solución del mismo.
+
+/*****************************************************
+/*****************************************************/
