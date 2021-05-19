f4f57ec001ade3f4c4b297e9330de3d1d54cea22
rubensato <rubensato@gmail.com>
2021-05-18 23:10:23 -0300

    resolucion ejercicio 3

--- ejercicios.js
+++ ejercicios.js
@@ -20,6 +20,8 @@ Agregar el enunciado de cada ejercicio como comentario en la solución del mismo
 Listado de ejercicios:
 ----------------------
 
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 1  <<<<<<<<<<<<<<<<<<<<<<<<<
+
 1) Variables y Operadores
     a) Crear dos variables numéricas y utilizar el operador suma para guardar el 
     valor de la suma de ambos números en una 3er variable.*/
@@ -57,7 +59,11 @@ Listado de ejercicios:
     cadena2 + '" (de longitud: ' + cadena2.length + '), es: ' + largoTotal);
     
 
+
+
 /*
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 2  <<<<<<<<<<<<<<<<<<<<<<<<<
+
 2) Strings
     a) Crear una variable de tipo string con al menos 10 caracteres y convertir 
     todo el texto en mayúscula (utilizar toUpperCase).*/
@@ -149,6 +155,187 @@ Listado de ejercicios:
 
 
 
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 3  <<<<<<<<<<<<<<<<<<<<<<<<<
+3) Arrays
+    a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log)*/
+
+    let arrayMeses1 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+    
+    console.log('3.a) El mes 5 es: ' + arrayMeses1[4] + ' , el mes 11 es: ' + arrayMeses1[10]);
+
+
+
+/*  b) Ordenar el array de meses alfabéticamente y mostrarlo por consola 
+    (utilizar sort).*/
+
+    let arrayMeses2 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+    
+    console.log('3.b) El array sin ordenar es: ' + arrayMeses2);
+    
+    console.log('El array ordenado es: ' + arrayMeses2.sort());
+
+
+
+/*  c) Agregar un elemento al principio y al final del array (utilizar unshift 
+    y push).*/
+
+    let arrayMeses3 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+    
+    console.log('3.c) El array original es: ' + arrayMeses3);
+
+    arrayMeses3.unshift("Primero");
+
+    arrayMeses3.push("Ultimo");
+    
+    console.log('El array modificado es: ' + arrayMeses3);
+
+
+    
+/*  d) Quitar un elemento del principio y del final del array (utilizar shift 
+    y pop).*/
+
+    let arrayMeses4 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+    
+    console.log('3.d) El array original es: ' + arrayMeses4);
+
+    arrayMeses4.shift();
+
+    arrayMeses4.pop();
+    
+    console.log('El array modificado es: ' + arrayMeses4);
+
+
+
+/*  e) Invertir el orden del array (utilizar reverse)*/
+
+    let arrayMeses5 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+
+    console.log('3.e) El array original es: ' + arrayMeses5);
+
+    console.log('El array invertido es: ' + arrayMeses5.reverse());
+   
+
+
+/*  f) Unir todos los elementos del array en un único string donde cada mes este 
+    separado por un guión - (utilizar join).*/
+
+    let arrayMeses6 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+
+    console.log('3.f) El array original es: ' + arrayMeses6);
+
+    var cadenaConGuiones = arrayMeses6.join('-');
+
+    console.log('El array como string de meses separados por guiones es: ' 
+    + cadenaConGuiones);
+   
+
+
+/*  g) Crear una copia del array de meses que contenga desde Mayo hasta 
+    Noviembre (utilizar slice).*/
+
+    let arrayMeses7 = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", 
+    "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", 
+    "Diciembre"];
+
+    console.log('3.f) El array original es: ' + arrayMeses7);
+
+    var mesesMayNov = arrayMeses7.slice(4,11);
+
+    console.log('El array mesesMayNov contiene: ' + mesesMayNov);
+       
+
+
+
+
+
+
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 4  <<<<<<<<<<<<<<<<<<<<<<<<<
+4) If Else
+    a) Crear un número aleatorio entre 0 y 1 utilizando la función 
+    Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con 
+    el mensaje “Greater than 0,5” y sino un alerta con el mensaje 
+    “Lower than 0,5”*/
+
+/*  b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y 
+    muestre los siguientes mensajes de alerta:
+        i) “Bebe” si la edad es menor a 2 años*/
+
+/*      ii) “Nino” si la edad es entre 2 y 12 años*/
+
+/*      iii) “Adolecente” si la edad es entre 13 y 19 años*/
+
+/*      iv) “Joven” si la edad está entre 20 y 30 años*/
+
+/*      v) “Adulto” entre 31 y 60 años*/
+
+/*      vi) “Adulto mayor” entre 61 y 75 años*/
+
+/*      vii) “Anciano” si es mayor a 75 años*/
+
+
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 5  <<<<<<<<<<<<<<<<<<<<<<<<<
+5) For
+    a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando 
+    un bucle for de JavaScript para mostrar una alerta utilizando cada una de 
+    las palabras.*/
+
+/*  b) Al array anterior convertir la primera letra de cada palabra en mayúscula 
+    y mostrar una alerta por cada palabra modificada.*/
+
+/*    c) Crear una variable llamada “sentence” que tenga un string vacío, luego 
+    al array del punto a) recorrerlo con un bucle for para ir guardando cada 
+    palabra dentro de la variable sentence. Al final mostrar una única alerta 
+    con la cadena completa.*/
+
+/*    d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el 
+    array con el número de la repetición, es decir que al final de la ejecución 
+    del bucle for deberia haber 10 elementos dentro del array, desde el número 0 
+    hasta al numero 9. Mostrar por la consola del navegador el al array final 
+    (utilizar console.log).*/
+    
+
+
+
+/*
+>>>>>>>>>>>>>>>>>>>>>>  EJERCICIO 6  <<<<<<<<<<<<<<<<<<<<<<<<<
+6) Funciones
+    a) Crear una función suma que reciba dos valores numéricos y retorne el 
+    resultado. Ejecutar la función y guardar el resultado en una variable, mostrando
+    el valor de dicha variable en la consola del navegador.*/
+
+/*  b) A la función suma anterior, agregarle una validación para controlar si alguno
+    de los parámetros no es un número, mostrar una alerta aclarando que uno de los 
+    parámetros tiene error y retornar el valor NaN como resultado.*/
+
+/*  c) Crear una función validate integer que reciba un número como parámetro y 
+    devuelva verdadero si es un número entero.*/
+
+/*  d) A la función suma del ejercicio 6b) agregarle una llamada que valide que los 
+    números sean enteros. En caso que haya decimales mostrar un alerta con el error 
+    y retorna el número convertido a entero (redondeado).*/
+
+/*  e) Convertir la validación del ejercicio 6b) en una función separada y llamarla 
+    dentro de la función suma probando que todo siga funcionando igual.*/
 
 
     
